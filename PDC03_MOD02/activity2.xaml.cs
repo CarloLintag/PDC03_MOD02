f9cc2de24d02ce6d08bd48d22b72e1199f654ec9
CarloLintag <carlolintag99@gmail.com>
2022-04-25 13:01:53 +0800

    Add project files.

---
+++ PDC03_MOD02/activity2.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MOD02
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class activity2 : ContentPage
+    {
+        public activity2()
+        {
+            InitializeComponent();
+        }
+
+        void changedSliderValue(object sender, ValueChangedEventArgs args)
+        {
+            labelValue.Text = args.NewValue.ToString("F3");
+        }
+
+        async void clickedButton(object sender, EventArgs args)
+        {
+            Button button = (Button)sender;
+            await DisplayAlert("Submitted",
+                "Activity 2 has been submitted", "OK");
+        }
+
+    }
+}
